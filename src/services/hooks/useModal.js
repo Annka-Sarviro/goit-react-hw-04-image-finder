418d15adc0439cb2b026455dfdf991e615178c9f
Ann Sarviro <Annka_nuha@i.ua>
2022-07-23 03:56:07 +0300

    add main

--- src/services/hooks/useModal.js
+++
@@ -1,9 +0,0 @@
-const useModal = (initialState = false) => {
-  const [isOpen, setIsOpen] = useState(initialState);
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
-
-  return { isOpen, openModal, closeModal };
-};
-
-export default useModal;
